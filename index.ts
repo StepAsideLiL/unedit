00555886ef01c9ea6089d47c16fdd06936878ed8
Rifat Khan <54655388+StepAsideLiL@users.noreply.github.com>
2026-01-25 23:35:33 +0600

    update editor as function instance.

--- index.ts
+++ index.ts
@@ -1,25 +1,15 @@
-function main() {
+function Editor() {
   const editorELement = document.querySelector("#editor") as HTMLElement;
 
-  if (!editorELement) return;
-
-  editorELement.innerText = "Hello, World!";
-  editorELement.contentEditable = "true";
-
-  const selection = document.getSelection();
-
-  function handle(e: Event) {
-    if (e.type === "selectionchange") {
-      if (!selection || selection.rangeCount === 0) return;
-      const range = selection.getRangeAt(0);
-      console.log(range.startOffset, range.endOffset, selection.direction);
-    }
-  }
-
-  document.addEventListener("selectionchange", handle);
-  editorELement.addEventListener("input", handle);
-  editorELement.addEventListener("keyup", handle);
-  editorELement.addEventListener("mouseup", handle);
+  return {
+    id: Math.random().toString(36).slice(2, 7),
+    init() {
+      if (!editorELement) return;
+      editorELement.innerText = "Hello, World!";
+      editorELement.contentEditable = "true";
+    },
+  };
 }
 
-main();
+const editor = Editor();
+editor.init();
